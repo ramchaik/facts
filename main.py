87a6e9bf9ad023adb093d0cbe16f689edd782111
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-06-01 11:14:56 -0300

    feat: integrate text splitter with text loader
    - chunking the text with 0 overlap and separator using 0

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0,
+)
 
-print(docs)
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+# print(docs)
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
 
